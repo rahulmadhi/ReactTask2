c99e29315dfe27e1af2157fa8c1834fd6e307f75
Rahul Madhi <49587770+rahulmadhi@users.noreply.github.com>
2023-02-24 16:49:27 +0530

    forms added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 import Apicalls from "./Components/Apicalls";
 import "./global.css";
 import Form from "./Form.jsx";
-import DisplayModal from './Components/DisplayModal';
+import DisplayModal from "./Components/DisplayModal";
+import Login from "./Components/Login.jsx";
+import Register from "./Components/RegisterPage.jsx";
 
 function App() {
   return (
     <>
       {/* <Apicalls /> */}
       {/* <Form /> */}
-      <DisplayModal />
+      {/* <DisplayModal /> */}
+      <Login />
+      {/* <Register /> */}
     </>
   );
 }
